d7a954c1df375e08f2f8bea89d71cbfc6c1b8e7a
Izmailov Mykyta <lampysk8@gmail.com>
2022-04-06 10:52:55 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function () {
+   $('.carousel__inner').slick({
+      speed: 1200,
+      // adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/left.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/icons/right.svg"></button>',
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               dots: true,
+               arrows: false
+            }
+         },
+         {
+            breakpoint: 600,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2
+            }
+         },
+         {
+            breakpoint: 480,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1
+            }
+         }
+      ]
+   });
+});
