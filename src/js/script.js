f29a68d5cfad9001dbe6ed0b1a09b5ef7c087aca
Izmailov Mykyta <lampysk8@gmail.com>
2022-04-17 13:57:32 +0300

    finished catalog section

--- src/js/script.js
+++ src/js/script.js
@@ -33,4 +33,37 @@ $(document).ready(function () {
          }
       ]
    });
+
+   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+      $(this)
+         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+   });
+
+
+   function toggleSlide(item) {
+      $(item).each(function (i) {
+         $(this).on('click', function (e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+         });
+      });
+   }
+   toggleSlide('.catalog-item__link');
+   toggleSlide('.catalog-item__back');
 });
+
+// import { tns } from "./node_modules/tiny-slider/src/tiny-slider";
+
+
+// var slider = tns({
+//    container: '.carousel__inner',
+//    items: 1,
+//    slideBy: 'page',
+//    autoplay: false,
+//    controlsText: [
+//       '<img src="img/icons/left.svg">',
+//       '<img src="img/icons/right.svg">'
+//    ]
+// });
